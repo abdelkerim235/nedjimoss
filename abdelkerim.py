082479e8964fc098a24fa8b8dea7a8e42586b07f
abdelkerim235 <125025025+abdelkerim235@users.noreply.github.com>
2023-02-12 01:53:03 +0300

    Add files via upload

--- abdelkerim.py
+++ abdelkerim.py
@@ -1,8 +1,20 @@
 a1=int(input("Enter the value of a:"))
 a2=int(input("Enter the value of b:"))
-a3=int(input("Enter thr value of c:"))
+a3=int(input("Enter the value of c:"))
+
 import math
-e=math.sqrt((a2**2)-(4*a1*a3))
-x1=(-a2+e)/(2*a1)
-x2=(-a2-e)/(2*a1)
-print("the roots of x are:",x1,"and",x2)
+h=((y**2)-(4*x*2))
+
+if h==0:
+
+    e=math.sqrt(h)
+    x1=(-a2+e)/(2*a1)
+    x2=(-a2-e)/(2*a1)
+    print("the roots of x is:",x1)
+elif h<0:
+    print("no result")
+else:
+    e=math.sqrt(h)
+    x1 = (-a2 + e) / (2 * a1)
+    x2 = (-a2 - e) / (2 * a1)
+    print("the roots of x are:",x1,"and",x2)
